e2849470db96a07e3bd807cccf26b3b842a81bb2
jcalle7 <jlcalle.2@sudamericano.edu.ec>
2025-07-24 14:57:28 -0500

    first commit

---
+++ src/test/java/com/turnos/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnos.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
